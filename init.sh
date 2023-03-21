f90a99bfdaf51bb8fb9646fc402cba0ff9df67a0
Andreas Håkansson <andreas@thecodejunkie.com>
2023-03-21 07:50:13 +0100

    chore: remove init.sh after executing

--- init.sh
+++ init.sh
@@ -6,6 +6,9 @@ CURRENT_DIRECTORY="${PWD##*/}"
 # Change the name of the ino file to match the directory name
 mv esp8266-mqtt-boilerplate.ino "$CURRENT_DIRECTORY".ino
 
+# Remove init script
+rm init.sh
+
 # Commit the change
 git add "$CURRENT_DIRECTORY".ino
 git commit -m "initial commit"
