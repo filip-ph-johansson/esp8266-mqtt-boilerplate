e89fa36f1647e4f4d2fa31a71b09547e28b290f8
filip johansson <filip.ph.johansson@gmail.com>
2023-03-28 08:17:59 +0200

    feat: change placeholder values on init

--- init.sh
+++ init.sh
@@ -6,6 +6,10 @@ CURRENT_DIRECTORY="${PWD##*/}"
 # Change the name of the ino file to match the directory name
 mv esp8266-mqtt-boilerplate.ino "$CURRENT_DIRECTORY".ino
 
+# Change placeholder values
+sed "s/esp8266-mqtt-boilerplate/$CURRENT_DIRECTORY/g" config.template.h > config.template.h.tmp
+mv  config.template.h.tmp  config.template.h
+
 # Commit the change
-git add "$CURRENT_DIRECTORY".ino
-git commit -m "initial commit"
+git add .
+git commit -m "initialize project"
